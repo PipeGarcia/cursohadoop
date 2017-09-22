9a5f53c377c4cc76c71f19fe4d6ab39d93b2eb01
Edwin Montoya Munera <emontoya@eafit.edu.co>
2017-09-22 08:22:27 -0500

    hbase track

--- 04-hbase/src/hbase/HBaseListTables.java
+++ 04-hbase/src/hbase/HBaseListTables.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 
-public class CreateListTables
+public class HBaseListTables
 {
   public static void main(String[] args) throws Exception
   {
